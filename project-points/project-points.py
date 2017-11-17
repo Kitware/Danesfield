0e89eb0e4695801c52ff317cdeec5dd64eb31083
Dan Lipsa <dan.lipsa@kitware.com>
2017-11-17 13:37:43 -0500

    Remove unnecessary code.

--- project-points/project-points.py
+++ project-points/project-points.py
@@ -1,4 +1,3 @@
-# Opening the gdal file
 import gdal
 import math
 import numpy
@@ -13,68 +12,22 @@ if (len(sys.argv) < 4):
 imageFileName = sys.argv[1]
 pointsFileName = sys.argv[2]
 destImageFileName = sys.argv[3]
+
+# open the GDAL file
 sourceImage = gdal.Open(imageFileName, gdal.GA_ReadOnly)
-
-# Getting Dataset Information
-print("Driver: {}/{}".format(sourceImage.GetDriver().ShortName,
-                             sourceImage.GetDriver().LongName))
-print("Size is {} x {} x {}".format(sourceImage.RasterXSize,
-                                    sourceImage.RasterYSize,
-                                    sourceImage.RasterCount))
-print("Projection is {}".format(sourceImage.GetProjection()))
-geotransform = sourceImage.GetGeoTransform()
-if geotransform:
-    print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
-    print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
-
 rpcMetaData = sourceImage.GetMetadata('RPC')
 
-
-# Fetching a Raster Band
-band = sourceImage.GetRasterBand(1)
-print("Band Type={}".format(gdal.GetDataTypeName(band.DataType)))
-
-min = band.GetMinimum()
-max = band.GetMaximum()
-if not min or not max:
-    (min,max) = band.ComputeRasterMinMax(True)
-print("Min={:.3f}, Max={:.3f}".format(min,max))
-
-if band.GetOverviewCount() > 0:
-    print("Band has {} overviews".format(band.GetOverviewCount()))
-
-if band.GetRasterColorTable():
-    print("Band has a color table with {} entries".format(band.GetRasterColorTable().GetCount()))
-
-
-# Test if the driver supports Create() and CreateCopy()
+# Test if the driver supports CreateCopy()
 fileformat = "NITF"
 driver = gdal.GetDriverByName(fileformat)
 metadata = driver.GetMetadata()
 destImage = None
-if metadata.get(gdal.DCAP_CREATE) == "YES":
-    print("Driver {} supports Create() method.".format(fileformat))
-    # destImage = driver.Create(destImageFileName, xsize=sourceImage.RasterXSize,
-    #                        ysize = sourceImage.RasterYSize, bands=1, eType=gdal.GDT_Float32)
-
-    # raster = numpy.zeros((sourceImage.RasterYSize, sourceImage.RasterXSize), dtype=numpy.float32)
-    # #raster = numpy.zeros((3000, 3000), dtype=numpy.uint16)
-    # raster[0:3000,0:3000] = 25
-    # raster[3000:6000,3000:6000] = 50
-    # raster[6000:9000,6000:9000] = 75
-    # raster[9000:12000,9000:12000] = 100
-    # destImage.GetRasterBand(1).WriteArray(raster)
-    # destImage = None
-
-
 if metadata.get(gdal.DCAP_CREATECOPY) == "YES":
-    print("Driver {} supports CreateCopy() method.".format(fileformat))
     destImage = driver.CreateCopy(destImageFileName, sourceImage, strict=0)
-
+    # create an emtpy image
     raster = numpy.zeros((sourceImage.RasterYSize, sourceImage.RasterXSize), dtype=numpy.uint16)
-    #raster = numpy.zeros((3000, 3000), dtype=numpy.uint16)
-
-if (destImage is None):
+else:
+    print("Driver {} does not supports CreateCopy() method.".format(fileformat))
     exit(0)
 
 # read the pdal file and project the points
@@ -109,7 +62,10 @@ for i in range(0, len(arrayX)):
     quantizedZ = arrayZ[i] * 60000 / (maxZ - minZ)
     rpcPoint = model.project(point)
     raster[int(rpcPoint[0]), int(rpcPoint[1])] = quantizedZ
-# close the gdal files
+
+# Write the image
 destImage.GetRasterBand(1).WriteArray(raster)
+
+# close the gdal files
 destImage = None
 sourceImage = None
