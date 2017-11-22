f2ed487afd3223cd0df66990055ac167237dc517
Xu Zhang <spongezhang@gmail.com>
2017-11-22 13:19:33 -0500

    rewrite the code a little bit

--- my_test/align.py
+++ my_test/align.py
@@ -7,46 +7,61 @@ if os.environ.get('DISPLAY','') == '':
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 import plotly.plotly as py
+import argparse
 
-imageFileName = '../data/test_warped.tiff'
-scale = 0.2
 
-color_image = cv2.imread(imageFileName) 
+def ProjectPoint(model, pt):
+    px = int((pt[0]-model['corners'][0])/model['project_model'][1]*model['scale'])
+    py = int((pt[1]-model['corners'][1])/model['project_model'][5]*model['scale'])
+    return [px,py]
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--input_img', default='../data/test_warped.tiff', help='folder to output log')
+parser.add_argument('--input_osm', default='../data/map.osm', help='folder to output log')
+parser.add_argument('--scale' , type=float, default=0.2, help='beta hyperparameter value')
+
+args = parser.parse_args()
+
+input_img = args.input_img
+scale = args.scale
+
+color_image = cv2.imread(input_img) 
 print(color_image.shape)
 small_color_image = np.zeros((int(color_image.shape[0]*scale),\
         int(color_image.shape[1]*scale), 3))
-small_color_image = cv2.resize(color_image,None,fx=scale, fy=scale)
-#cv2.resize(color_image, small_color_image)
-color_image = small_color_image
+
+if scale != 1.0:
+    small_color_image = cv2.resize(color_image, None, fx=scale, fy=scale)
+    color_image = small_color_image
+
 grayimg = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 edge_img = cv2.Canny(grayimg, 100, 200)
 cv2.imwrite('edge.jpg',edge_img)
 
 # open the GDAL file
-sourceImage = gdal.Open(imageFileName, gdal.GA_ReadOnly)
+sourceImage = gdal.Open(input_img, gdal.GA_ReadOnly)
 rpcMetaData = sourceImage.GetMetadata('RPC')
-myarray = np.array(sourceImage.GetRasterBand(1).ReadAsArray())
-myarray = myarray*0+255
+myarray = np.zeros((sourceImage.RasterYSize,sourceImage.RasterXSize))
 
 gt = sourceImage.GetGeoTransform() # captures origin and pixel size
 print('Origin:', (gt[0], gt[3]))
 print('Pixel size:', (gt[1], gt[5]))
-
-gdal.ApplyGeoTransform(gt,0,0)
 
 left = gdal.ApplyGeoTransform(gt,0,0)[0]
 top = gdal.ApplyGeoTransform(gt,0,0)[1]
 right = gdal.ApplyGeoTransform(gt,sourceImage.RasterXSize,sourceImage.RasterYSize)[0]
 bottom = gdal.ApplyGeoTransform(gt,sourceImage.RasterXSize,sourceImage.RasterYSize)[1]
 
-print(left, top, right, bottom)
-osmFileName = '../data/map.osm'
-#osmFileName = '../data/state/cb_2016_us_state_20m.shp'
+model = {}
+model['corners'] = [left, top, right, bottom]
+model['project_model'] = gt 
+model['scale'] = scale
 
-ds = ogr.Open(osmFileName)
+ds = ogr.Open(args.input_osm)
 layer = ds.GetLayerByIndex(3)
-
 nameList = []
+
 draw_flag = False
 for feature in layer:
     if feature.GetField("building") != None:  # only streets
@@ -69,20 +84,15 @@ for feature in layer:
             if flag:
                 poly_point_list = []
                 tmp_img = np.zeros((int(color_image.shape[0]),\
-                    int(color_image.shape[1]),3))
-                print(tmp_img.shape)
+                    int(color_image.shape[1])))
+
                 for i in range(0, polygon.GetPointCount()):
                     pt = polygon.GetPoint(i)
-                    poly_point_list.append([int((pt[0]-left)/gt[1]*scale),\
-                            int((pt[1]-top)/gt[5]*scale)])
-                #poly_point_list.append([0,0])
-                #poly_point_list.append([0,100])
-                #poly_point_list.append([100,100])
-                #poly_point_list.append([100,0])
-                #poly_point_list.append([0,0])
+                    poly_point_list.append(ProjectPoint(model,pt))
+
                 poly_array = np.array(poly_point_list)
                 poly_array = poly_array.reshape((-1,1,2))
-                cv2.polylines(tmp_img,[poly_array],True,(255,255,255),thickness=2)
+                cv2.polylines(tmp_img,[poly_array],True,(255),thickness=2)
                 #cv2.imshow('image',tmp_img)
                 #cv2.imwrite('contour.jpg',tmp_img)
                 #cv2.waitKey(0)
@@ -90,14 +100,15 @@ for feature in layer:
 
                 for y in range(0,tmp_img.shape[0]):
                     for x in range(0,tmp_img.shape[1]):
-                        if tmp_img[y,x,0] > 200:
+                        if tmp_img[y,x] > 200:
                             check_point_list.append([x,y])
+
                 print(len(check_point_list))
+
                 max_value = 0
                 offsetx = 0
                 offsety = 0
                 for dy in range(-40,40,2):
-                    #for j in range(-40,40,1):
                     for dx in range(-40,40,2):
                         total_value = 0
                         for pt in check_point_list:
@@ -110,8 +121,6 @@ for feature in layer:
                 print(max_value)
                 print(offsetx, offsety)
                 for i in range(len(poly_array)):
-                    #poly_array[i,0,0] = int((poly_array[i,0,0] + offsetx)/scale)
-                    #poly_array[i,0,1] = int((poly_array[i,0,1] + offsety)/scale)
                     poly_array[i,0,0] = int((poly_array[i,0,0] + offsetx))
                     poly_array[i,0,1] = int((poly_array[i,0,1] + offsety))
 
@@ -119,14 +128,19 @@ for feature in layer:
                 cv2.imwrite('new_contour.jpg',tmp_img)
                 cv2.imshow('image',tmp_img)
                 cv2.waitKey(0)
+
+                #for i in range(len(poly_array)):
+                    #poly_array[i,0,0] = int((poly_array[i,0,0] + offsetx)/scale)
+                    #poly_array[i,0,1] = int((poly_array[i,0,1] + offsety)/scale)
                 #cv2.fillPoly(myarray,[poly_array],True,(0))
+
                 draw_flag = True
     #if draw_flag:
     #    break
                 
 #print(nameList)
 ds.Destroy()
-cv2.imwrite('../data/mask.png',myarray)
+#cv2.imwrite('../data/mask.png',myarray)
 #imgplot = plt.imshow(myarray)
 print(myarray.shape)
 print(myarray.dtype)
