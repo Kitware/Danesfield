ef652d8fe15482cc4a48bfc2d8051db215659eff
David Stoup <david.stoup@kitware.com>
2017-12-06 09:44:44 -0500

    Flake8 fixes to cropping util

--- tools/crop_images.py
+++ tools/crop_images.py
@@ -17,32 +17,30 @@
 #  limitations under the License.
 ###############################################################################
 
-import gdalconst
-import gdalnumeric
-
 # from gaia.geo.gdal_functions import *
 # from gaia.geo.processes_raster import *
 
 import os
-import fnmatch
-
-import traceback
-import sys
 import json
 
 import numpy as np
 
 from danesfield import rpc
 from danesfield import raytheon_rpc
-from PIL import Image, ImageDraw
 
+import gdalconst
+import gdalnumeric
 import gdal
 import ogr
 
+from PIL import Image, ImageDraw
+
+
 def gdal_get_transform(src_image):
     geo_trans = src_image.GetGeoTransform()
-    if geo_trans==(0.0, 1.0, 0.0, 0.0, 0.0, 1.0):
+    if geo_trans == (0.0, 1.0, 0.0, 0.0, 0.0, 1.0):
         geo_trans = gdal.GCPsToGeoTransform(src_image.GetGCPs())
+
     return geo_trans
 
 
@@ -198,152 +196,149 @@ for root, dirs, files in os.walk(src_root_dir):
         if ext != ".ntf":
             continue
 
-        try:
-            src_img_file = os.path.join(root, file_)
-            dst_img_file = os.path.join(new_root, file_)
-            dst_file_no_ext = os.path.splitext(dst_img_file)[0]
-            dst_img_file = dst_file_no_ext + ".tif"
+        src_img_file = os.path.join(root, file_)
+        dst_img_file = os.path.join(new_root, file_)
+        dst_file_no_ext = os.path.splitext(dst_img_file)[0]
+        dst_img_file = dst_file_no_ext + ".tif"
 
-            src_image = gdal.Open(src_img_file, gdalconst.GA_ReadOnly)
-            geo_trans = gdal_get_transform(src_image)
+        print('Converting img: ' + src_img_file)
+        src_image = gdal.Open(src_img_file, gdalconst.GA_ReadOnly)
+        geo_trans = gdal_get_transform(src_image)
 
-            nodata_values = []
-            nodata = 0
-            for i in range(src_image.RasterCount):
-                nodata_value = src_image.GetRasterBand(i+1).GetNoDataValue()
-                if not nodata_value:
-                    nodata_value = nodata
-                nodata_values.append(nodata_value)
+        nodata_values = []
+        nodata = 0
+        for i in range(src_image.RasterCount):
+            nodata_value = src_image.GetRasterBand(i+1).GetNoDataValue()
+            if not nodata_value:
+                nodata_value = nodata
+            nodata_values.append(nodata_value)
 
-            polygon_json = {"type":
-                                "Polygon", "coordinates":
-                                [[[ul_lon, ul_lat], [ur_lon, ur_lat],
-                                  [lr_lon, lr_lat], [ll_lon, ll_lat],
-                                  [ul_lon, ul_lat]]]}
+        polygon_json = {"type":
+                        "Polygon", "coordinates":
+                            [[[ul_lon, ul_lat], [ur_lon, ur_lat],
+                              [lr_lon, lr_lat], [ll_lon, ll_lat],
+                              [ul_lon, ul_lat]]]}
 
-            polygon_json = json.dumps(polygon_json)
-            poly = ogr.CreateGeometryFromJson(polygon_json)
-            min_x, max_x, min_y, max_y = poly.GetEnvelope()
-            rpc_md = src_image.GetMetadata('RPC')
-            model = rpc.rpc_from_gdal_dict(rpc_md)
-            if (corrected_rpc_dir):
-                updated_rpc = read_raytheon_RPC(corrected_rpc_dir, file_)
-                if updated_rpc is None:
-                    print ('No RPC file exists for image file: ' + src_img_file)
-                else:
-                    model = updated_rpc
-                    rpc_md = rpc.rpc_to_gdal_dict(updated_rpc)
-
-            pixelPoly = world_to_pixel_poly(model, poly)
-
-            ul_x, lr_x, ul_y, lr_y = map(int, pixelPoly.GetEnvelope())
-            ul_x = max(0, ul_x)
-            ul_y = max(0, ul_y)
-            lr_x = min(src_image.RasterXSize - 1, lr_x)
-            lr_y = min(src_image.RasterYSize - 1, lr_y)
-
-            samp_off = rpc_md['SAMP_OFF']
-            samp_off = float(samp_off) - ul_x
-            rpc_md['SAMP_OFF'] = str(samp_off)
-
-            line_off = rpc_md['LINE_OFF']
-            line_off = float(line_off) - ul_y
-            rpc_md['LINE_OFF'] = str(line_off)
-
-            # Calculate the pixel size of the new image
-            # Constrain the width and height to the bounds of the image
-            px_width = int(lr_x - ul_x + 1)
-            if px_width + ul_x > src_image.RasterXSize - 1:
-                px_width = int(src_image.RasterXSize - ul_x - 1)
-
-            px_height = int(lr_y - ul_y + 1)
-            if px_height + ul_y > src_image.RasterYSize - 1:
-                px_height = int(src_image.RasterYSize - ul_y - 1)
-
-            # We've constrained x & y so they are within the image.
-            # If the width or height ends up negative at this point,
-            # the AOI is completely outside the image
-            if px_width < 0 or px_height < 0:
-                continue
-
-            # Load the source data as a gdalnumeric array
-            clip = src_image.ReadAsArray(ul_x, ul_y, px_width, px_height)
-            src_dtype = clip.dtype
-
-            # Create a new geomatrix for the image
-            geo_trans = list(geo_trans)
-            geo_trans[0] = min_x
-            geo_trans[3] = max_y
-
-            # Map points to pixels for drawing the
-            # boundary on a blank 8-bit,
-            # black and white, mask image.
-            raster_poly = Image.new("L", (px_width, px_height), 1)
-            rasterize = ImageDraw.Draw(raster_poly)
-            geometry_count = poly.GetGeometryCount()
-            for i in range(0, geometry_count):
-                points = []
-                pixels = []
-                pts = poly.GetGeometryRef(i)
-                if pts.GetPointCount() == 0:
-                    pts = pts.GetGeometryRef(0)
-                for p in range(pts.GetPointCount()):
-                    points.append((pts.GetX(p), pts.GetY(p)))
-                for p in points:
-                    pixels.append(world_to_pixel(geo_trans, p[0], p[1]))
-                rasterize.polygon(pixels, 0)
-
-            # create output raster
-            raster_band = src_image.GetRasterBand(1)
-            output_driver = gdal.GetDriverByName('MEM')
-
-            # In the event we have multispectral images,
-            # shift the shape dimesions we are after,
-            # since position 0 will be the number of bands
-            clip_shp_0 = clip.shape[0]
-            clip_shp_1 = clip.shape[1]
-            if clip.ndim > 2:
-                clip_shp_0 = clip.shape[1]
-                clip_shp_1 = clip.shape[2]
-
-            output_dataset = output_driver.Create(
-                '', clip_shp_1, clip_shp_0,
-                src_image.RasterCount, raster_band.DataType)
-
-            # Copy All metadata data from src to dst
-            domains = src_image.GetMetadataDomainList()
-            for tag in domains:
-                md = src_image.GetMetadata(tag)
-                if md:
-                    output_dataset.SetMetadata(md, tag)
-
-            # Rewrite the rpc_md that we modified above.
-            output_dataset.SetMetadata(rpc_md, 'RPC')
-            gdalnumeric.CopyDatasetInfo(src_image, output_dataset,
-                                        xoff=ul_x, yoff=ul_y)
-
-            bands = src_image.RasterCount
-            if bands > 1:
-                for i in range(bands):
-                    outBand = output_dataset.GetRasterBand(i + 1)
-                    outBand.SetNoDataValue(nodata_values[i])
-                    outBand.WriteArray(clip[i])
+        polygon_json = json.dumps(polygon_json)
+        poly = ogr.CreateGeometryFromJson(polygon_json)
+        min_x, max_x, min_y, max_y = poly.GetEnvelope()
+        rpc_md = src_image.GetMetadata('RPC')
+        model = rpc.rpc_from_gdal_dict(rpc_md)
+        if (corrected_rpc_dir):
+            updated_rpc = read_raytheon_RPC(corrected_rpc_dir, file_)
+            if updated_rpc is None:
+                print('No RPC file exists for image file: ' + src_img_file)
             else:
-                outBand = output_dataset.GetRasterBand(1)
-                outBand.SetNoDataValue(nodata_values[0])
-                outBand.WriteArray(clip)
+                model = updated_rpc
+                rpc_md = rpc.rpc_to_gdal_dict(updated_rpc)
 
-            if dst_img_file:
-                output_driver = gdal.GetDriverByName('GTiff')
-                outfile = output_driver.CreateCopy(dst_img_file, output_dataset, False)
+        pixelPoly = world_to_pixel_poly(model, poly)
 
-                # We need to write this data out after the CreateCopy call or it's lost
-                # This change seems to happen in GDAL with python 3
-                output_dataset.SetGeoTransform(geo_trans)
-                output_dataset.SetProjection(gdal_get_projection(src_image))
-                outfile = None
+        ul_x, lr_x, ul_y, lr_y = map(int, pixelPoly.GetEnvelope())
+        ul_x = max(0, ul_x)
+        ul_y = max(0, ul_y)
+        lr_x = min(src_image.RasterXSize - 1, lr_x)
+        lr_y = min(src_image.RasterYSize - 1, lr_y)
 
-        except:
-            print ('Problem cropping image: ' + src_img_file)
-            print (traceback.format_exc())
+        samp_off = rpc_md['SAMP_OFF']
+        samp_off = float(samp_off) - ul_x
+        rpc_md['SAMP_OFF'] = str(samp_off)
 
+        line_off = rpc_md['LINE_OFF']
+        line_off = float(line_off) - ul_y
+        rpc_md['LINE_OFF'] = str(line_off)
+
+        # Calculate the pixel size of the new image
+        # Constrain the width and height to the bounds of the image
+        px_width = int(lr_x - ul_x + 1)
+        if px_width + ul_x > src_image.RasterXSize - 1:
+            px_width = int(src_image.RasterXSize - ul_x - 1)
+
+        px_height = int(lr_y - ul_y + 1)
+        if px_height + ul_y > src_image.RasterYSize - 1:
+            px_height = int(src_image.RasterYSize - ul_y - 1)
+
+        # We've constrained x & y so they are within the image.
+        # If the width or height ends up negative at this point,
+        # the AOI is completely outside the image
+        if px_width < 0 or px_height < 0:
+            continue
+
+        # Load the source data as a gdalnumeric array
+        clip = src_image.ReadAsArray(ul_x, ul_y, px_width, px_height)
+        src_dtype = clip.dtype
+
+        # Create a new geomatrix for the image
+        geo_trans = list(geo_trans)
+        geo_trans[0] = min_x
+        geo_trans[3] = max_y
+
+        # Map points to pixels for drawing the
+        # boundary on a blank 8-bit,
+        # black and white, mask image.
+        raster_poly = Image.new("L", (px_width, px_height), 1)
+        rasterize = ImageDraw.Draw(raster_poly)
+        geometry_count = poly.GetGeometryCount()
+        for i in range(0, geometry_count):
+            points = []
+            pixels = []
+            pts = poly.GetGeometryRef(i)
+            if pts.GetPointCount() == 0:
+                pts = pts.GetGeometryRef(0)
+            for p in range(pts.GetPointCount()):
+                points.append((pts.GetX(p), pts.GetY(p)))
+            for p in points:
+                pixels.append(world_to_pixel(geo_trans, p[0], p[1]))
+            rasterize.polygon(pixels, 0)
+
+        # create output raster
+        raster_band = src_image.GetRasterBand(1)
+        output_driver = gdal.GetDriverByName('MEM')
+
+        # In the event we have multispectral images,
+        # shift the shape dimesions we are after,
+        # since position 0 will be the number of bands
+        clip_shp_0 = clip.shape[0]
+        clip_shp_1 = clip.shape[1]
+        if clip.ndim > 2:
+            clip_shp_0 = clip.shape[1]
+            clip_shp_1 = clip.shape[2]
+
+        output_dataset = output_driver.Create(
+            '', clip_shp_1, clip_shp_0,
+            src_image.RasterCount, raster_band.DataType)
+
+        # Copy All metadata data from src to dst
+        domains = src_image.GetMetadataDomainList()
+        for tag in domains:
+            md = src_image.GetMetadata(tag)
+            if md:
+                output_dataset.SetMetadata(md, tag)
+
+        # Rewrite the rpc_md that we modified above.
+        output_dataset.SetMetadata(rpc_md, 'RPC')
+        gdalnumeric.CopyDatasetInfo(src_image, output_dataset,
+                                    xoff=ul_x, yoff=ul_y)
+
+        bands = src_image.RasterCount
+        if bands > 1:
+            for i in range(bands):
+                outBand = output_dataset.GetRasterBand(i + 1)
+                outBand.SetNoDataValue(nodata_values[i])
+                outBand.WriteArray(clip[i])
+        else:
+            outBand = output_dataset.GetRasterBand(1)
+            outBand.SetNoDataValue(nodata_values[0])
+            outBand.WriteArray(clip)
+
+        if dst_img_file:
+            output_driver = gdal.GetDriverByName('GTiff')
+            outfile = output_driver.CreateCopy(
+                dst_img_file, output_dataset, False)
+
+            # We need to write this data out
+            # after the CreateCopy call or it's lost
+            # This change seems to happen in GDAL with python 3
+            output_dataset.SetGeoTransform(geo_trans)
+            output_dataset.SetProjection(gdal_get_projection(src_image))
+            outfile = None
