4f0b886ff913fe91160bc5b8c6fd3d527789185f
Dan Lipsa <dan.lipsa@kitware.com>
2018-04-26 13:09:42 -0400

    Create a list of orthorectified tiles from images and DSM tiles.

---
+++ tools/orthorectify-list.py
@@ -0,0 +1,146 @@
+from tools import ortho
+
+import argparse
+import gdal
+import glob
+import numpy
+import os.path
+import re
+import sys
+
+
+class OrthorectifyArgs:
+    source_image = ""
+    dsm = ""
+    destination_image = ""
+    occlusion_thresh = ""
+    denoise_radius = ""
+    raytheon_rpc = ""
+    def __str__(self):
+        ret = "orthorectify.py " + self.source_image + " " + self.dsm +\
+          " " + self.destination_image
+        if self.occlusion_thresh is not None:
+            ret = ret + " -t " + str(self.occlusion_thresh)
+        if self.denoise_radius is not None:
+            ret = ret + " -d " + str(self.denoise_radius)
+        ret = ret + " --raytheon-rpc " + self.raytheon_rpc if self.raytheon_rpc else ret
+        return ret
+
+
+def intersection(a, b):
+    x1 = max(a[0], b[0])
+    y1 = max(a[1], b[1])
+    x2 = min(a[2], b[2])
+    y2 = min(a[3], b[3])
+    if x1<x2 and y1<y2:
+        return [x1, x2, y1, y2]
+    # else return None
+
+parser = argparse.ArgumentParser(
+    description='Orthorectify a list of images to cover all DSMs. '
+    'The image chosen is the one with the smallest NITF_CSEXRA_OBLIQUITY_ANGLE '
+    'that (partially) covers the DSM. The output has the same name as the source image '
+    'with postfix or_pan (if image_folders contain PAN) or or_msi')
+parser.add_argument("dsm_folder",
+                    help="Folder for all DSMs which follow name_ii_jj.tif pattern, "
+                    "where ii and jj are indexes (00, 01, ...)")
+parser.add_argument("image_folders", nargs="+", help="Source image folders")
+parser.add_argument('-t', "--occlusion_thresh", type=float, default=1.0,
+                    help="Threshold on height difference for detecting "
+                    "and masking occluded regions (in meters)")
+parser.add_argument('-d', "--denoise_radius", type=float, default=2,
+                    help="Apply morphological operations with this radius "
+                    "to the DSM reduce speckled noise")
+parser.add_argument("--rpc_folder", type=str,
+                    help="Raytheon RPC folder. If not provided, "
+                    "the RPC is read from the source_image")
+parser.add_argument("--dense_ids", type=str,
+                    help="Process only the DSMs with the specified IDs. "
+                    "IDs are listed in the specified file using the format name_000ii_000jj."
+                    "Comments are prefixed by #. Otherwise process all DSMs in the image_folders.")
+args = parser.parse_args()
+
+imagesList = []
+for oneFolder in args.image_folders:
+    imagesInFolder = glob.glob(oneFolder + "/*.NTF")
+    imagesList.extend(imagesInFolder)
+
+images = numpy.array(imagesList)
+angles = numpy.zeros(len(images))
+bounds = numpy.zeros([len(images), 4])
+for i,f in enumerate(images):
+    sourceImage = gdal.Open(f, gdal.GA_ReadOnly)
+    metaData = sourceImage.GetMetadata()
+    angles[i] = metaData['NITF_CSEXRA_OBLIQUITY_ANGLE']
+    bounds[i] = ortho.bounding_box(sourceImage)
+# sort images by angle
+sortIndex = angles.argsort()
+images = images[sortIndex]
+angles = angles[sortIndex]
+bounds = bounds[sortIndex]
+# list of dsms
+dsmList = glob.glob(args.dsm_folder + "/dsm_*.tif")
+dsms = numpy.array(dsmList)
+sortIndex = dsms.argsort()
+dsms = dsms[sortIndex]
+
+# print("======================================================================")
+# for i,f in enumerate(images):
+#     print("{}: {} {}".format(f, angles[i], bounds[i]))
+# sys.exit(0)
+
+oargs = OrthorectifyArgs();
+oargs.occlusion_thresh = args.occlusion_thresh
+oargs.denoise_radius = args.denoise_radius
+
+reIndex = re.compile(r'\d+')
+
+ids = dsms
+if args.dense_ids:
+    f = open(args.dense_ids)
+    if not f:
+        sys.exit(10)
+    ids = [line for line in f if not line[0] == '#']
+    ids = [reIndex.findall(line) for line in ids]
+    ids = ["dsm_{}_{}.tif".format(line[0][-2:], line[1][-2:])  for line in ids]
+else:
+    ids = [os.path.basename(line) for line in dsms]
+ids = set(ids)
+
+for dsm in dsms:
+    dsmBasename = os.path.basename(dsm)
+    if not dsmBasename in ids:
+        print("Skipping {} not in dense_ids".format(dsmBasename))
+        continue
+    index = reIndex.findall(dsm)
+    oargs.dsm = dsm
+    dsmImage = gdal.Open(dsm, gdal.GA_ReadOnly)
+    dsmBounds = ortho.bounding_box(dsmImage)
+    areaDsm = (dsmBounds[2] - dsmBounds[0]) * (dsmBounds[3] - dsmBounds[1])
+    for i, source_image in enumerate(images):
+        print("========== {} {} {} ==========".format(index[0], index[1],
+                                                      angles[i]))
+        imageIntersectDsm = intersection(dsmBounds, bounds[i])
+        areaImageIntersectDsm = (imageIntersectDsm[2] - imageIntersectDsm[0]) *\
+            (imageIntersectDsm[3] - imageIntersectDsm[1]) if imageIntersectDsm else 0
+        if areaImageIntersectDsm < areaDsm * 3. / 4.:
+            print("{} {}".format(areaImageIntersectDsm, areaDsm))
+            print("{} {}".format(dsmBounds, bounds[i]))
+            continue
+        oargs.source_image = source_image
+        destination_image = os.path.basename(source_image)
+        destination_image = os.path.splitext(destination_image)[0]
+        if args.rpc_folder:
+            oargs.raytheon_rpc = glob.glob(
+                args.rpc_folder + "/GRA_" + destination_image + '*.up.rpc')[0]
+        postfix = "pan" if source_image.find('PAN') > 0 else "msi"
+        oargs.destination_image =\
+          destination_image + "_or_" + postfix + "_" + index[0] +\
+          "_" + index[1] + ".tif"
+        print(oargs)
+        if ortho.orthorectify(oargs) <= ortho.PARTIAL_DSM_INTERSECTION:
+            break
+        # We don't have a complete image so try the next image.
+        print("Image does not cover DSM {}".format(oargs.source_image))
+        if os.path.isfile(oargs.destination_image):
+            os.remove(oargs.destination_image)
