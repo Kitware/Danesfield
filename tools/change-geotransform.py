66fc4a20db75321ce7aad68bb10c74b0492adcc1
Matt Leotta <matt.leotta@kitware.com>
2018-05-16 14:24:05 -0400

    update change-geotransform.py to have a main function

--- tools/change-geotransform.py
+++ tools/change-geotransform.py
@@ -3,25 +3,35 @@
 import argparse
 import gdal
 
-parser = argparse.ArgumentParser(
-    description='Change the geotransform of an image')
-parser.add_argument("image", help="Image file name")
-parser.add_argument("--x_shift", type=float, help="The origin is shifted with this amount")
-parser.add_argument("--y_shift", type=float, help="The origin is shifted with this amount")
-args = parser.parse_args()
 
-image = gdal.Open(args.image, gdal.GA_Update)
-if not image:
-    exit(1)
+def main(args):
+    parser = argparse.ArgumentParser(
+        description='Change the geotransform of an image')
+    parser.add_argument("image", help="Image file name")
+    parser.add_argument("--x_shift", type=float, help="The origin is shifted with this amount")
+    parser.add_argument("--y_shift", type=float, help="The origin is shifted with this amount")
+    args = parser.parse_args(args)
 
-# check the default geotransform
-oldgeo=image.GetGeoTransform()
-geo = list(oldgeo)
-if (args.x_shift):
-    geo[0] = geo[0] + args.x_shift
-if (args.y_shift):
-    geo[3] = geo[3] + args.y_shift
+    image = gdal.Open(args.image, gdal.GA_Update)
+    if not image:
+        return False
 
-print("Changing geotransform from {} to {}".format(oldgeo, geo))
+    # check the default geotransform
+    oldgeo = image.GetGeoTransform()
+    geo = list(oldgeo)
+    if (args.x_shift):
+        geo[0] = geo[0] + args.x_shift
+    if (args.y_shift):
+        geo[3] = geo[3] + args.y_shift
 
-image.SetGeoTransform(geo)
+    print("Changing geotransform from {} to {}".format(oldgeo, geo))
+
+    image.SetGeoTransform(geo)
+    return True
+
+
+if __name__ == '__main__':
+    import sys
+    ret = main(sys.argv[1:])
+    if ret is False:
+        sys.exit(1)
