750b959ec53df62bd602f53b56618e180ed40518
Matt Leotta <matt.leotta@kitware.com>
2018-05-16 15:04:37 -0400

    flake8 fixes for intersects-polygon.py

--- tools/intersects-polygon.py
+++ tools/intersects-polygon.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
-from gaia.geo.geo_inputs import *
-from gaia.geo.processes_vector import *
-from gaia.geo.processes_raster import *
-from osgeo import gdal,osr
-from shutil import copyfile
-import subprocess
-from subprocess import call
+from gaia.geo.geo_inputs import VectorFileIO, FeatureIO
+from gaia.geo.processes_vector import IntersectsProcess
+from osgeo import gdal, osr
 
 
-
-def GetExtent(gt,cols,rows):
+def GetExtent(gt, cols, rows):
     ''' Return list of corner coordinates from a geotransform
 
         @type gt:   C{tuple/list}
@@ -20,22 +15,23 @@ def GetExtent(gt,cols,rows):
         @param cols: number of columns in the dataset
         @type rows:   C{int}
         @param rows: number of rows in the dataset
-        @rtype:    C{[float,...,float]}
+        @rtype:    C{[float, ..., float]}
         @return:   coordinates of each corner
     '''
-    ext=[]
-    xarr=[0,cols]
-    yarr=[0,rows]
+    ext = []
+    xarr = [0, cols]
+    yarr = [0, rows]
 
     for px in xarr:
         for py in yarr:
-            x=gt[0]+(px*gt[1])+(py*gt[2])
-            y=gt[3]+(px*gt[4])+(py*gt[5])
-            ext.append([x,y])
+            x = gt[0]+(px*gt[1])+(py*gt[2])
+            y = gt[3]+(px*gt[4])+(py*gt[5])
+            ext.append([x, y])
         yarr.reverse()
     return ext
 
-def ReprojectCoords(coords,src_srs,tgt_srs):
+
+def ReprojectCoords(coords, src_srs, tgt_srs):
     ''' Reproject a list of x,y coordinates.
 
         @type geom:     C{tuple/list}
@@ -47,11 +43,11 @@ def ReprojectCoords(coords,src_srs,tgt_srs):
         @rtype:         C{tuple/list}
         @return:        List of transformed [[x,y],...[x,y]] coordinates
     '''
-    trans_coords=[]
-    transform = osr.CoordinateTransformation( src_srs, tgt_srs)
-    for x,y in coords:
-        x,y,z = transform.TransformPoint(x,y)
-        trans_coords.append([x,y])
+    trans_coords = []
+    transform = osr.CoordinateTransformation(src_srs, tgt_srs)
+    for x, y in coords:
+        x, y, z = transform.TransformPoint(x, y)
+        trans_coords.append([x, y])
     return trans_coords
 
 
@@ -67,50 +63,43 @@ def main(args):
                              "overlapping the specified polygon")
     args = parser.parse_args(args)
 
-
     if (len(args.polygon) == 4):
-        xmin,ymin,xmax,ymax = [float(val) for val in args.polygon]
+        xmin, ymin, xmax, ymax = [float(val) for val in args.polygon]
         polygon = FeatureIO(features=[
             {"geometry":
                 {"type": "Polygon",
                  "coordinates":
-                 [
-                     [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
-                 ]
-                },
-             "properties": {"id": "Bounding box"}
-            },
-            ])
+                    [
+                        [[xmin, ymin], [xmax, ymin], [xmax, ymax], [xmin, ymax]]
+                    ]},
+             "properties": {"id": "Bounding box"}}, ])
     elif (len(args.polygon) == 1):
         polygonImage = gdal.Open(args.polygon[0], gdal.GA_ReadOnly)
         if (polygonImage):
-            gt=polygonImage.GetGeoTransform()
+            gt = polygonImage.GetGeoTransform()
             cols = polygonImage.RasterXSize
             rows = polygonImage.RasterYSize
-            ext=GetExtent(gt,cols,rows)
+            ext = GetExtent(gt, cols, rows)
 
-            src_srs=osr.SpatialReference()
+            src_srs = osr.SpatialReference()
             src_srs.ImportFromWkt(polygonImage.GetProjection())
             tgt_srs = src_srs.CloneGeogCS()
-            p = ReprojectCoords(ext,src_srs,tgt_srs)
+            p = ReprojectCoords(ext, src_srs, tgt_srs)
             polygon = FeatureIO(features=[
                 {"geometry":
                     {"type": "Polygon",
-                    "coordinates":
-                    [
+                     "coordinates":
+                     [
                         [[p[0][0], p[0][1]], [p[1][0], p[1][1]],
                          [p[2][0], p[2][1]], [p[3][0], p[3][1]]]
-                    ]
-                },
-                "properties": {"id": "Bounding box"}
-                },
-                ])
+                     ]},
+                 "properties": {"id": "Bounding box"}}, ])
         else:
             polygon = VectorFileIO(uri=args.polygon[0])
     else:
-        print("wrong number of parameters for polygon: {} (can be 4 or 1)".format(len(args.polygon)))
+        print("wrong number of parameters for polygon: {} "
+              "(can be 4 or 1)".format(len(args.polygon)))
         return False
-
 
     source = VectorFileIO(uri=args.source)
     destination = VectorFileIO(uri=args.destination)
