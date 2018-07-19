3a4f4f81493aa0f6c5a50e0994a912b6e8528b9b
zhixin li <lizhixin2887@gmail.com>
2018-07-19 10:56:37 -0400

    Upload New File

---
+++ danesfield/surface/poly_functions.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+
+import re
+import numpy as np
+from shapely.geometry import Polygon
+
+
+def list_intersect(a, b):
+    """ return the intersection of two lists """
+    return list(set(a) & set(b))
+
+
+def list_union(a, b):
+    """ return the union of two lists """
+    return list(set(a) | set(b))
+
+
+def ply_parser(fp):
+    '''
+    :param fp: PLY file path
+    :return: Surface coordinates and surface index
+    '''
+    tf = open(fp)
+    lines = tf.readlines()
+    flag = 0
+    for l in lines:
+        if re.search("\s*element\s*vertex\s*\d*", l) != None:
+            vertex_num = int(re.findall("\d+\.?\d*", l)[0])
+        if re.search("\s*element\s*face\s*\d*", l) != None:
+            face_num = int(re.findall("\d+\.?\d*", l)[0])
+        if re.search("end_header", l) != None:
+            begin_num = flag + 1
+        flag += 1
+
+    x = [float(re.findall("-*\d+\.?\d*", l)[0]) for l in lines[begin_num:begin_num + vertex_num]]
+    y = [float(re.findall("-*\d+\.?\d*", l)[1]) for l in lines[begin_num:begin_num + vertex_num]]
+    z = [float(re.findall("-*\d+\.?\d*", l)[2]) for l in lines[begin_num:begin_num + vertex_num]]
+
+    cor = [[x[i], y[i], z[i]] for i in range(0, len(x))]
+    cor = np.asarray(cor)
+    f = [re.findall("\d+\.?\d*", l) for l in lines[begin_num + vertex_num:begin_num + vertex_num + face_num]]
+
+    return cor, f
+
+
+def check_relation(plane1, plane2):
+    '''
+    Checking spatial relationship between planes.
+    :param plane1:
+    :param plane2:
+    :return: spatial relationship tag
+    '''
+    p1 = Polygon(plane1)
+    p2 = Polygon(plane2)
+    if p1.intersects(p2):
+        if p1.contains(p2):
+            flag = 1
+        else:
+            if p1.area >= p2.area:
+                flag = 2
+            else:
+                flag = 3
+    else:
+        flag = 4
+    return flag
+
+
+def get_height_from_dem(cor, dem):
+    '''
+    Get Z coordinate from DEM based on given XY coordinate.
+    r1-r4 represent the image boundaries for coordinates outside.
+    :param cor: XY coordinate
+    :param dem: DEM object
+    :return: Z coordinate
+    '''
+    transform = dem.GetGeoTransform()
+    xOrigin = transform[0]
+    yOrigin = transform[3]
+    pixelWidth = transform[1]
+    pixelHeight = transform[5]
+    band = dem.GetRasterBand(1)
+    data = band.ReadAsArray()
+    base_height = []
+    r1 = [[0, i] for i in range(data.shape[1])]
+    r2 = [[data.shape[0] - 1, i] for i in range(data.shape[1])]
+    r3 = [[i, 0] for i in range(data.shape[0])]
+    r4 = [[i, data.shape[1] - 1] for i in range(data.shape[0])]
+    r = np.r_[r1, r2, r3, r4]
+    for i in range(cor.shape[0]):
+        x = cor[i, 0]
+        y = cor[i, 1]
+        xOffset = int((x - xOrigin) / pixelWidth)
+        yOffset = int((y - yOrigin) / pixelHeight)
+        try:
+            value = data[yOffset][xOffset]
+            base_height.append(value)
+        except:
+            dist_2 = np.sum((r - np.array([yOffset, xOffset])) ** 2, axis=1)
+            index = np.argmin(dist_2)
+            value = data[r[index, 0]][r[index, 1]]
+            base_height.append(value)
+    return np.array(base_height)
+
+
+def get_height_from_lower_surface(plane1, plane2):
+    '''
+    :param plane1: Higher surface
+    :param plane2: Lower surface
+    :return: Z coordinate on lower surface
+    '''
+    [a, b, c, d] = fit_plane(plane1)
+    z = lambda x: -(a * x[0] + b * x[1] + d) / c
+    return z([plane2[:, 0], plane2[:, 1]])
+
+
+def get_difference_plane(plane1, plane2):
+    '''
+    Get difference and intersection part for two planes
+    :param plane1:
+    :param plane2:
+    :return:
+    '''
+    p1 = Polygon(plane1)
+    p2 = Polygon(plane2)
+    pd = p2.difference(p1)
+    pi = p2.intersection(p1)
+    flag = True
+    try:
+        p3 = np.array(pd.exterior.coords[:])
+        p4 = np.array(pi.exterior.coords[:])
+    except:
+        flag = False
+        p3 = None
+        p4 = None
+
+    return [flag, p3, p4]
+
+
+def fit_plane(point):
+    '''
+    Using normal vector and distance to origin to represent a plane.
+    :param point: Plane coordinates
+    :return: Plane parameters
+    '''
+    xyz_mean = np.array([point[:, 0].mean(), point[:, 1].mean(), point[:, 2].mean()])
+    xyz_m = np.array([point[:, 0] - xyz_mean[0], point[:, 1] - xyz_mean[1], point[:, 2] - xyz_mean[2]])
+    [U, S, V] = np.linalg.svd(xyz_m)
+    v = np.array([U[0, 2], U[1, 2], U[2, 2]])
+    a = v[0]
+    b = v[1]
+    c = v[2]
+    d = - np.dot(v, xyz_mean.transpose())
+    # normal vector of plane
+    return [a, b, c, d]
+
+
+def rotate_plane(plane):
+    '''
+    Rotate a 3D plane into 2D plane.
+    :param plane:
+    :return: [2D plane coordinates, rotate tag(whether or not), rotation matrix, plane center]
+    '''
+    temp_cor = plane
+    p_n = fit_plane(temp_cor)
+    p_n = np.array(p_n[0:3])
+    s_n = np.array([0, 0, 1])
+    [rx, ry, rz] = np.cross(p_n, s_n)
+    ra = np.arccos(np.dot(p_n, s_n) / (np.linalg.norm(p_n) * np.linalg.norm(s_n)))
+    rotate_flag = False
+    rm = None
+    center = None
+    if abs(ra) > 0.001:
+        norm = np.linalg.norm(np.cross(p_n, s_n))
+        [rx, ry, rz] = [rx / norm, ry / norm, rz / norm]
+        r1 = [np.cos(ra) + rx ** 2 * (1 - np.cos(ra)), rx * ry * (1 - np.cos(ra)) - rz * np.sin(ra),
+              ry * np.sin(ra) + rx * rz * (1 - np.cos(ra))]
+        r2 = [rz * np.sin(ra) + rx * ry * (1 - np.cos(ra)), np.cos(ra) + ry ** 2 * (1 - np.cos(ra)),
+              -rx * np.sin(ra) + ry * rz * (1 - np.cos(ra))]
+        r3 = [-ry * np.sin(ra) + rx * rz * (1 - np.cos(ra)), rx * np.sin(ra) + ry * rz * (1 - np.cos(ra)),
+              np.cos(ra) + rz ** 2 * (1 - np.cos(ra))]
+        rm = np.array([r1, r2, r3])
+        center = [np.mean(temp_cor[:, 0]), np.mean(temp_cor[:, 1]), np.mean(temp_cor[:, 2])]
+        cor_2d = np.dot(rm, (temp_cor - center).transpose()).transpose()
+        rotate_flag = True
+    else:
+        cor_2d = temp_cor
+
+    return [cor_2d, rotate_flag, rm, center]
+
+
+def remove_close_point(plane, T = 0.05):
+    '''
+    Remove close points in a surface
+    :param plane:
+    :param T: Threshold
+    :return: New plane coordinates
+    '''
+    test_plane = plane[:, 0:2]
+    del_list = []
+    for i in range(0, test_plane.shape[0]):
+        for j in range(i+1, test_plane.shape[0]):
+            dist = np.linalg.norm(test_plane[i] - test_plane[j])
+            if dist <= T:
+                del_list.append(i)
+    plane = np.delete(plane, del_list, axis=0)
+    return plane
+
+
+def fix_intersection(plane):
+    '''
+    Solve self-intersection issue
+    :param plane: plane coordinates
+    :return: None self-intersection plane coordinates
+    '''
+    temp_cor = plane
+    p_n = fit_plane(temp_cor)
+    p_n = np.array(p_n[0:3])
+    s_n = np.array([0, 0, 1])
+    [rx, ry, rz] = np.cross(p_n, s_n)
+    ra = np.arccos(np.dot(p_n, s_n) / (np.linalg.norm(p_n) * np.linalg.norm(s_n)))
+    rotate_flag = 0
+    if abs(ra) > 0.001:
+        norm = np.linalg.norm(np.cross(p_n, s_n))
+        [rx, ry, rz] = [rx / norm, ry / norm, rz / norm]
+        r1 = [np.cos(ra) + rx ** 2 * (1 - np.cos(ra)), rx * ry * (1 - np.cos(ra)) - rz * np.sin(ra),
+              ry * np.sin(ra) + rx * rz * (1 - np.cos(ra))]
+        r2 = [rz * np.sin(ra) + rx * ry * (1 - np.cos(ra)), np.cos(ra) + ry ** 2 * (1 - np.cos(ra)),
+              -rx * np.sin(ra) + ry * rz * (1 - np.cos(ra))]
+        r3 = [-ry * np.sin(ra) + rx * rz * (1 - np.cos(ra)), rx * np.sin(ra) + ry * rz * (1 - np.cos(ra)),
+              np.cos(ra) + rz ** 2 * (1 - np.cos(ra))]
+        rm = np.array([r1, r2, r3])
+        center = [np.mean(temp_cor[:, 0]), np.mean(temp_cor[:, 1]), np.mean(temp_cor[:, 2])]
+        cor_2d = np.dot(rm, (temp_cor - center).transpose()).transpose()
+        rotate_flag = 1
+    else:
+        cor_2d = temp_cor
+    poly_cor = Polygon(cor_2d[:, 0:2])
+    if poly_cor.is_valid:
+        return remove_close_point(plane)
+    else:
+        t_cor = poly_cor.buffer(0, join_style=2)
+        poly_cor = t_cor.buffer(0, join_style=2)
+        if poly_cor.geom_type == 'MultiPolygon':
+            area = [poly_cor[geo_index].area for geo_index in range(0, len(poly_cor))]
+            temp_cor = np.array(poly_cor[area.index(max(area))].exterior.coords[:])
+        else:
+            temp_cor = np.array(poly_cor.exterior.coords[:])
+
+        temp_cor = np.c_[temp_cor, np.zeros(temp_cor.shape[0])]
+        if rotate_flag == 1:
+            temp_cor = np.dot(np.linalg.inv(rm), temp_cor.transpose()).transpose() + center
+        else:
+            temp_cor[:, 2] = temp_cor[:, 2] + np.mean(plane[:, 2])
+        return remove_close_point(temp_cor)
+
+
+def fix_height(plane, new_cor):
+    '''
+    Fit a un-flat plane by given coordinates and plane parameters
+    :param plane: Plane parameters
+    :param new_cor: Given plane XY coordinates
+    :return: Fixed plane coordinates
+    '''
+    [a, b, c, d] = fit_plane(plane)
+    z = lambda x: -(a * x[0] + b * x[1] + d) / c
+    height = z([new_cor[:, 0], new_cor[:, 1]])
+    new_plane = np.c_[new_cor[:, 0], new_cor[:, 1], height]
+    return new_plane
