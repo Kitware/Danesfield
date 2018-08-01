9188005aa19cab44539e7b2d0d43e0b529b383c1
Xu Zhang <spongezhang@gmail.com>
2018-08-01 12:11:05 -0400

    add additional files

---
+++ danesfield/geon_fitting/tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -0,0 +1,11 @@
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+
+# TF1.2
+#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+#
+
+
+#
+## TF1.4
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /home/xuzhang/tool/cuda-9.0/include -I $TF_INC/external/nsync/public  -L /home/xuzhang/tool/cuda-9.0/lib64/ -lcudart -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
