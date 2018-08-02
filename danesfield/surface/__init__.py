0a9bcc8aa7cf19d837784968d300e8d2bd63efd0
Zhixin Li <zhixin.li@kitware.com>
2018-08-02 16:09:14 -0400

    Update danesfield/surface/__init__.py

--- danesfield/surface/__init__.py
+++ danesfield/surface/__init__.py
@@ -1,10 +1,1 @@
 #!/usr/bin/env python
-from .base_surface import Surface
-from .base_surface import Building
-from .scene import Model
-from .curve_surface import Curved_building
-from .sphere import Sphere_building
-from .poly_functions import *
-from .geon_functions import *
-from .geon import Geon
-from .MinimumBoundingBox import MinimumBoundingBox
