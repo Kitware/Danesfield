a3a6222504d83ab27bb3afb09d53c7be02e47635
David Joy <david.joy@admins-mbp.kitware.com>
2018-11-01 14:40:49 -0400

    Added option to compute bounds from image for get_road_vector

--- tools/run_danesfield.py
+++ tools/run_danesfield.py
@@ -124,7 +124,6 @@ def main(args):
                                      config['paths']['imagery_dir'])
 
     aoi_name = config['aoi']['name']
-    left, bottom, right, top = (config['aoi'][d] for d in ('left', 'bottom', 'right', 'top'))
 
     gsd = float(config['params'].get('gsd', 0.25))
 
@@ -264,10 +263,7 @@ def main(args):
     # Query OpenStreetMap for road vector data
     logging.info('---- Fetching OSM road vector ----')
     road_vector_output_fpath = os.path.join(working_dir, 'road_vector.geojson')
-    cmd_args = ['--left', left,
-                '--bottom', bottom,
-                '--right', right,
-                '--top', top,
+    cmd_args = ['--bounding-img', dsm_file,
                 '--output-dir', working_dir]
     script_call = ["get_road_vector.py"] + cmd_args
     print(*script_call)
