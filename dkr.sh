7224a46fa5a827617ae69c1ecc4de99a9fee66c7
Eugene Borovikov <eugene.borovikov@kitware.com>
2021-05-03 14:43:32 -0400

    additional mount points, e.g. user home folder, working folder

--- dkr.sh
+++ dkr.sh
@@ -1,1 +1,6 @@
-nvidia-docker run -it --rm -v /data/core3D-data:/mnt --gpus all --shm-size 8G kitware/danesfield /bin/bash
+nvidia-docker run -it --rm --gpus all --shm-size 8G\
+ -v /data/core3D-data:/mnt\
+ -v $HOME:/home/$USER\
+ -v $HOME/work/danesfield:/work\
+ kitware/danesfield\
+ /bin/bash
