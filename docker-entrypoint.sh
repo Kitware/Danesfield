3f567ffa882cb781844c876ab82b95bba2a4c72f
Dan Lipsa <dan.lipsa@kitware.com>
2022-01-12 20:54:04 -0500

    Run command under conda environment.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
+# logging
+echo "Execute docker-entrypoint.sh"
+
 # Make 'conda' command available to shell environment
 source /opt/conda/etc/profile.d/conda.sh
 
@@ -13,4 +16,4 @@ Xvfb :1 -screen 0 1024x768x16 -nolisten tcp > xvfb.log &
 export DISPLAY=:1.0
 
 # Run specified command
-exec "$@"
+eval "$@"
