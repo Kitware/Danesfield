7476c7504857a819af85aed660af2720e446dbd7
Dan Lipsa <dan.lipsa@kitware.com>
2022-09-13 17:53:06 -0400

    Fixes.

--- tools/tiler.py
+++ tools/tiler.py
@@ -181,7 +181,7 @@ def quantize(obj_path: str, tiff_files: List[str], property_names: List[str], te
                     ta[ta > features_percentile_range[tiff_index][1]] = features_percentile_range[tiff_index][1]
                     ta[ta < features_percentile_range[tiff_index][0]] = features_percentile_range[tiff_index][0]
 
-                fr = (np.min(ta), np.max(ta))
+                fr = (np.nanmin(ta), np.nanmax(ta))
                 features_range[tiff_index] = (min(features_range[tiff_index][0], fr[0]),
                                               max(features_range[tiff_index][1], fr[1]))
                 fr = features_range[tiff_index]
@@ -225,8 +225,8 @@ def quantize(obj_path: str, tiff_files: List[str], property_names: List[str], te
         print("writing png: {}".format(dir + "/" + quantized_file_basename + ".png"))
         png_writer.SetFileName(dir + "/" + quantized_file_basename + ".png")
         png_writer.Write()
-    property_texture_file = None
-    if generate_json:
+    property_texture_file = ""
+    if generate_json and quantized_files:
         property_texture_file = dir + "/property_texture.json"
         with open(property_texture_file, "w") as outfile:
             json.dump(pt, outfile, indent=4)
@@ -240,10 +240,11 @@ def read_buildings_obj(
     Builds a multiblock dataset (similar with one built by the CityGML reader)
     from a list of OBJ files. It can generate quantized png textures from property
     textures stored in tiff files. It generates a property texture description
-    for the first OBJ - the assumption is that the property textures are the same for
-    all textures in the dataset.
+    for the dataset.
     It expects textures in order PNGs first and then TIFFs.
     """
+    # sort files by numerical value
+    files.sort(key=lambda f: int(re.sub('\\D', '', f)))
     if number_of_features == UNINITIALIZED and \
        begin_feature_index != UNINITIALIZED and end_feature_index != UNINITIALIZED:
         feature_index_range = range(begin_feature_index, end_feature_index)
@@ -252,13 +253,25 @@ def read_buildings_obj(
     root = vtkMultiBlockDataSet()
     property_texture_file = None
     feature_percentile_range: List[Tuple[float, float]] = []
+    number_of_tiff_files = 0
     if (quantization_percentile < 100):
         all_tiff_values: List[List[float]] = []
         for i in feature_index_range:
             dir = os.path.dirname(files[i])
             (png_files, tiff_files, property_names) = get_obj_texture_file_names(files[i], property_texture_png_index)
             if i == 0:
-                all_tiff_values = [[] for _ in range(len(tiff_files))]
+                number_of_tiff_files = len(tiff_files)
+                if number_of_tiff_files == 0:
+                    logging.error("Passing quantization_percentile implies "
+                                  "there are TIFF files but we didn't fine any")
+                    return (None, None)
+                all_tiff_values = [[] for _ in range(number_of_tiff_files)]
+            else:
+                if not number_of_tiff_files == len(tiff_files):
+                  logging.error("Different number of TIFF files for first feature: {} "
+                                "and for feature {}: {}".format(
+                                    number_of_tiff_files, i, len(tiff_files)))
+                  return (None, None)
             for j in range(len(tiff_files)):
                 (tiff_array, _) = read_tiff(dir + "/" + tiff_files[j])
                 if i == 0:
@@ -272,6 +285,7 @@ def read_buildings_obj(
                  np.nanpercentile(all_tiff_values[j], quantization_percentile + (100 - quantization_percentile) / 2)))
             print("percentile range {} : {}".format(j, feature_percentile_range[j]))
     features_range: List[Tuple[float, float]] = []
+    number_of_tiff_files = 0
     for i in feature_index_range:
         reader = vtkOBJReader()
         reader.SetFileName(files[i])
@@ -282,13 +296,16 @@ def read_buildings_obj(
             continue
         (png_files, tiff_files, property_names) = get_obj_texture_file_names(files[i], property_texture_png_index)
         if i == 0:
+            number_of_tiff_files = len(tiff_files)
             gdal_utils.read_offset(files[i], file_offset)
             for _ in range(len(tiff_files)):
                 features_range.append((sys.float_info.max, -sys.float_info.max))
-        if len(features_range) != len(tiff_files):
-            logging.error("Number of properties for feature {} is different than "
-                          "for feature 0: {} versus {}".format(
-                              i, len(tiff_files), len(features_range)))
+        else:
+            if not number_of_tiff_files == len(tiff_files):
+                logging.error("Different number of TIFF files for first feature: {} "
+                              "and for feature {}: {}".format(
+                                  number_of_tiff_files, i, len(tiff_files)))
+                return (None, None)
         (quantized_files, ptf) = quantize(files[i], tiff_files, property_names, len(png_files), features_range, feature_percentile_range, i == len(feature_index_range) - 1)
         if ptf:
             property_texture_file = ptf
