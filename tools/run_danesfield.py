a632824f90a32d54ee76d5b9d3adf5a25df02247
Eugene Borovikov <Eugene.Borovikov@Kitware.com>
2022-11-29 18:32:57 -0500

    add tension_mod option

--- tools/run_danesfield.py
+++ tools/run_danesfield.py
@@ -254,6 +254,11 @@ def main(args):
     parser.add_argument('--tiles', action='store_true', help='run tiler to get tiles')
     parser.add_argument('--vNDVI', action='store_true',
                         help='run pipeline with visible NDVI computation using RGB-colored point cloud')
+    parser.add_argument('-t:m', '--tension_mod', action='store_true',
+                        help='modify tension for each level?')
+    parser.add_argument('--tension', metavar='T', type=int, default=15,
+                        help='Number of inner smoothing iterations for DTM, '
+                             'greater values increase surface tension; default=%(default)s')
     args = parser.parse_args(args)
 
     # Read configuration file
@@ -386,6 +391,9 @@ def main(args):
 
     cmd_args = py_cmd(relative_tool_path('fit_dtm.py'))
     cmd_args += [dsm_file, dtm_file]
+
+    if args.tension_mod:
+        cmd_args.append('--tension_mod')
 
     run_step(fit_dtm_outdir,
              'fit-dtm',
